972a85eceaa4fff297f0b6609267910dfd185735
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-15 23:18:07 +0530

    socket code fix

--- src/socket.js
+++ src/socket.js
@@ -2,18 +2,18 @@ import { io } from "socket.io-client";
 
 export const initSocket = async () => {
   const options = {
-      'force new connection': true,
-      reconnectionAttempt: 'Infinity',
-      timeout: 10000,
-      transports: ['websocket'],
+    "force new connection": true,
+    reconnectionAttempt: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
   };
   try {
-      const socket = io('http://localhost:5000', options);
-      await new Promise((resolve) => socket.on('connect', resolve));
-      console.log("Socket connected successfully");
-      return socket;
+    const socket = io("https://codecollab-ry3w.onrender.com/api", options);
+    await new Promise((resolve) => socket.on("connect", resolve));
+    console.log("Socket connected successfully");
+    return socket;
   } catch (error) {
-      console.error("Error connecting to socket:", error);
-      throw error;
+    console.error("Error connecting to socket:", error);
+    throw error;
   }
-}
+};
