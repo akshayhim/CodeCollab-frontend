60dbf2da094d9c1abab41fb17a501c6c914a6a56
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-15 23:54:43 +0530

    testing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import { FaInstagram } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Contact() {
   return (
@@ -11,36 +11,56 @@ export default function Contact() {
         <div>
           Contribute to this project here :{" "}
           <span className="underline">
-            <Link href="https://shorturl.at/mtEMZ" target="_blank">
+            {/* <a href="https://shorturl.at/mtEMZ" target="_blank"> */}
+            <a
+              href="https://shorturl.at/mtEMZ"
+              target="_blank"
+              rel="noreferrer"
+            >
               Github Repo
-            </Link>
+            </a>
+            Github Repo
+            {/* </a> */}
           </span>
         </div>
         <div className="flex items-center">
           <span className="mr-2">Contact me:</span>
           <span className="flex gap-4">
             <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-              <Link href="mailto:akshayhimat@gmail.com" target="_blank">
+              <a
+                href="mailto:akshayhimat@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <BiLogoGmail size={20} />
-              </Link>
+              </a>
             </div>
             <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-              <Link href="https://github.com/akshayhim" target="_blank">
+              <a
+                href="https://github.com/akshayhim"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <AiFillGithub size={20} />
-              </Link>
+              </a>
             </div>
             <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-              <Link
+              <a
                 href="https://www.linkedin.com/in/akshay-himatsingka/"
                 target="-blank"
+                rel="noreferrer"
               >
                 <AiFillLinkedin size={20} />
-              </Link>
+              </a>
             </div>
             <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-              <Link href="https://www.instagram.com/_akshay.h/" target="-blank">
+              <a
+                href="https://www.instagram.com/_akshay.h/"
+                target="-blank"
+                rel="noreferrer"
+              >
                 <FaInstagram size={20} />
-              </Link>
+              </a>
             </div>
           </span>
         </div>
