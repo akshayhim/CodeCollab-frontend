5e71d6dd47a69ce21ec7f939152010beacfa1aae
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-16 16:32:00 +0530

    design changes

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -7,6 +7,13 @@ import "codemirror/theme/elegant.css";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/display/placeholder.js";
+import "codemirror/addon/lint/lint";
+import "codemirror/addon/lint/javascript-lint";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/search/search";
+import "codemirror/addon/search/searchcursor";
+import "codemirror/addon/search/jump-to-line";
+import "codemirror/addon/selection/active-line";
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
   const editorRef = useRef(null);
@@ -20,6 +27,11 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
           autoCloseTags: true,
           autoCloseBrackets: true,
           lineNumbers: true,
+          lineWrapping: true,
+          matchBrackets: true,
+          styleActiveLine: true,
+          lint: true, // Enable linting
+          gutters: ["CodeMirror-lint-markers"],
           placeholder:
             "Invite people to this room by sharing the room ID and start coding...",
         }
