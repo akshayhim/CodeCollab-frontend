f2c2d96a08e53f03ca7cea778c44395bc6e43ba8
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-16 11:22:04 +0530

    fixes

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <div className="min-h-[calc(100vh-7.16rem)] flex flex-col items-center justify-center bg-gray-100">
+    <div className="flex flex-col items-center justify-center bg-gray-100">
       <Registration />
-      <Link to="/register" href="#" className="body2">
-        {"Don't have an account? "}
-        <span className="underline">Sign Up</span>
+      <Link to="/login" href="#" className="body2">
+        {"Already have an account? "}
+        <span className="underline">Log In</span>
       </Link>
     </div>
   );
