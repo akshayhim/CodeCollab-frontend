6a9932b6db28bb991a98ac5bf6c1d6dc6b2f28d4
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-16 16:39:34 +0530

    bug fix

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -15,6 +15,7 @@ import "codemirror/addon/search/searchcursor";
 import "codemirror/addon/search/jump-to-line";
 import "codemirror/addon/selection/active-line";
 
+
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
   const editorRef = useRef(null);
   useEffect(() => {
@@ -31,7 +32,7 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
           matchBrackets: true,
           styleActiveLine: true,
           lint: true, // Enable linting
-          gutters: ["CodeMirror-lint-markers"],
+          gutters: ['CodeMirror-lint-markers'],
           placeholder:
             "Invite people to this room by sharing the room ID and start coding...",
         }
