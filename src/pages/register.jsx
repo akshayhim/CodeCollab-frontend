66fd4e995955f207e22963098bcfed6c96c51c59
Akshay Himatsingka <akshayhimat@gmail.com>
2023-12-16 11:16:19 +0530

    minor changes

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,15 @@
 // import React from 'react';
-import Registration from '../components/Registration';
+import Registration from "../components/Registration";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
     <div>
       <Registration />
+      <Link to="/register" href="#" className="body2">
+        {"Don't have an account? "}
+        <span className="underline">Sign Up</span>
+      </Link>
     </div>
   );
 };
